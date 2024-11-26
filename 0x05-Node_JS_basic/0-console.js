f13517eac172d88da01bcd44a845132fe6fa2dea
robert2030 <robert7ashioya@gmail.com>
2024-11-26 10:14:03 +0300

    zero console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,12 @@
-const displayMessage = (d) => console.log(d);
+/** 0. Executing basic javascript with Node JS
+
+In the file 0-console.js, create a function named
+displayMessage that prints in STDOUT the string argument.
+*/
+
+function displayMessage(message) {
+  // print message to STDOUT
+  console.log(message);
+}
 
 module.exports = displayMessage;
